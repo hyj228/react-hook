1e94a0abae59d63295297c00aedc08c680f7ebed
霍艳杰 <huoyanjie@mianchao-inc.com>
2019-12-27 14:43:15 +0800

    打包

---
+++ build/precache-manifest.4e0658efd14777db3b0e9b70b995aa8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36fe5b09273c61119cc325aa6c02e51b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3717067259af0403fe2d",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5a0ac2ffd285a0967e48",
+    "url": "/static/js/2.994c0125.chunk.js"
+  },
+  {
+    "revision": "3717067259af0403fe2d",
+    "url": "/static/js/main.056a421b.chunk.js"
+  },
+  {
+    "revision": "be5d8b4b11dc89f4b328",
+    "url": "/static/js/runtime-main.1bcbe76a.js"
+  }
+]);
